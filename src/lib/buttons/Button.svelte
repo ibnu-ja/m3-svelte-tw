413e1a4ddbb268d3e33afc4f20bc6944fceb6804
Ibnu JA <ibnu.ja@outlook.com>
2025-04-26 09:39:48 +0000

    Button.svelte cleanup

--- src/lib/buttons/Button.svelte
+++ src/lib/buttons/Button.svelte
@@ -7,16 +7,6 @@
   import Primitive from "$lib/primitive/Primitive.svelte";
   import type { PrimitiveProps } from "$lib/primitive";
 
-  /*
-  *   import type { HTMLButtonAttributes } from "svelte/elements";
-  import Layer from "$lib/misc/Layer.svelte";
-  export let display = "inline-flex";
-  export let extraOptions: HTMLButtonAttributes = {};
-  export let iconType: "none" | "left" | "full" = "none";
-  export let type: "elevated" | "filled" | "tonal" | "outlined" | "text";
-  export let disabled = false;
-  * */
-
   interface ButtonProps extends PrimitiveProps {
     iconType?: ButtonVariantProps["iconType"]
     type?: ButtonVariantProps["type"]
