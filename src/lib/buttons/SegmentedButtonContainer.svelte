619ce05e94c6344adf19e32e2c0e248ac7dce501
Ibnu JA <ibnu.ja@outlook.com>
2025-04-26 15:00:10 +0000

    rename var

--- src/lib/buttons/SegmentedButtonContainer.svelte
+++ src/lib/buttons/SegmentedButtonContainer.svelte
@@ -4,11 +4,11 @@
   import type { SvelteHTMLElements } from "svelte/elements";
   import Primitive from "$lib/primitive/Primitive.svelte";
 
-  interface FABProps extends PrimitiveProps {
+  interface SegmentedButtonContainerProps extends PrimitiveProps {
     class?: SvelteHTMLElements["div"]["class"];
   }
 
-  let { children, class: className, ...attrs }: FABProps = $props();
+  let { children, class: className, ...attrs }: SegmentedButtonContainerProps = $props();
 </script>
 
 <Primitive as="div" class={cn("inline-flex border border-outline h-10 rounded-full overflow-hidden [&>input]:absolute [&>input]:opacity-0 [&>input]:pointer-events-none", className)} {...attrs}>
