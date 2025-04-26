8fa3a70d0e7a6180be10c50bd193291f30abd770
Ibnu JA <ibnu.ja@outlook.com>
2025-04-26 09:22:21 +0000

    fix side sheet button color

--- src/lib/containers/StandardSideSheet.svelte
+++ src/lib/containers/StandardSideSheet.svelte
@@ -10,7 +10,7 @@
 <div class="m3-container flex">
   <div class="header">
     <span class="m3-font-title-large">{headline}</span>
-    <Button iconType="full" onclick={() => onclose()}>
+    <Button class="text-on-surface-variant" iconType="full" type="text" onclick={() => onclose()}>
       <Icon icon={iconX} />
     </Button>
   </div>
@@ -28,9 +28,5 @@
         justify-content: space-between;
 
         padding: 1.5rem;
-    }
-
-    .header > :global(button.m3-container) {
-        color: rgb(var(--m3-scheme-on-surface-variant));
     }
 </style>
