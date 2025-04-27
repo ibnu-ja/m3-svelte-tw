414a9d69d6f843b242c16041d4301ac2a34b51a3
ibnu-ja <ibnu.ja@outlook.com>
2025-04-27 22:39:27 +0700

    add button/segmented button (#3)
    * enable docpage
    
    * update SegmentedButtonContainer.svelte
    
    * rename var
    
    * refactor SegmentedButtonItem.svelte
    
    * update progress

--- src/lib/buttons/SegmentedButtonContainer.svelte
+++ src/lib/buttons/SegmentedButtonContainer.svelte
@@ -1,27 +1,16 @@
 <script lang="ts">
-  import type { HTMLAttributes } from "svelte/elements";
-  export let display = "inline-flex";
-  export let extraOptions: HTMLAttributes<HTMLDivElement> = {};
+  import { cn } from "$lib/misc/utils";
+  import type { PrimitiveProps } from "$lib/primitive";
+  import type { SvelteHTMLElements } from "svelte/elements";
+  import Primitive from "$lib/primitive/Primitive.svelte";
+
+  interface SegmentedButtonContainerProps extends PrimitiveProps {
+    class?: SvelteHTMLElements["div"]["class"];
+  }
+
+  let { children, class: className, ...attrs }: SegmentedButtonContainerProps = $props();
 </script>
 
-<div class="m3-container" style="display: {display};" {...extraOptions}>
-  <slot />
-</div>
-
-<style>
-  :root {
-    --m3-segmented-button-shape: var(--m3-util-rounding-full);
-  }
-
-  .m3-container {
-    border: 0.0625rem solid rgb(var(--m3-scheme-outline));
-    height: 2.5rem;
-    border-radius: var(--m3-segmented-button-shape);
-    overflow: hidden;
-  }
-  .m3-container > :global(input) {
-    position: absolute;
-    opacity: 0;
-    pointer-events: none;
-  }
-</style>
+<Primitive as="div" class={cn("inline-flex border border-outline h-10 rounded-full overflow-hidden [&>input]:absolute [&>input]:opacity-0 [&>input]:pointer-events-none", className)} {...attrs}>
+  {@render children?.()}
+</Primitive>
