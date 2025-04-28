af271e90d0b4254e500e3144e1306e8ecee263b4
Ibnu <ibnu.ja@outlook.com>
2025-04-28 17:03:15 +0700

    update Card.svelte

--- src/lib/containers/Card.svelte
+++ src/lib/containers/Card.svelte
@@ -1,51 +1,98 @@
 <script lang="ts">
-  import type { HTMLAttributes, HTMLButtonAttributes } from "svelte/elements";
+  import type { HTMLAttributes, MouseEventHandler } from "svelte/elements";
+  import Primitive from "$lib/primitive/Primitive.svelte";
+  import type { PrimitiveProps } from "$lib/primitive";
+  import { type CardVariantProps, cardVariants } from "$lib/containers/index";
+  import type { Snippet } from "svelte";
+  import { cn } from "$lib/misc/utils";
+  import { Layer } from "$lib";
 
-  export let display = "flex";
-  export let extraOptions: HTMLAttributes<HTMLDivElement> & HTMLButtonAttributes = {};
-  export let type: "elevated" | "filled" | "outlined";
+  interface CardProps extends PrimitiveProps {
+    type?: CardVariantProps["type"];
+    clickable?: boolean;
+    onclick?: MouseEventHandler<any> | null;
+    class?: HTMLAttributes<any>["class"];
+    children?: Snippet;
+  }
+
+  let {
+    type = "elevated",
+    as = "div",
+    clickable = false,
+    class: className,
+    onclick,
+    children,
+    ...attrs
+  }: CardProps = $props();
+
+  $effect(() => {
+    if (clickable) {
+      as = "button";
+    }
+  });
+
+  const preventDefault = (
+    fn: ((this: Window, ev: MouseEvent) => any) | null,
+  ): ((this: Window, ev: MouseEvent) => void) | null => {
+    if (!fn) return null;
+    return function (this: Window, ev: MouseEvent): void {
+      ev.preventDefault();
+      fn.call(this, ev);
+    };
+  };
 </script>
 
-<div class="m3-container type-{type}" style="display: {display};" {...extraOptions}>
-  <slot />
-</div>
+<Primitive
+  {as}
+  class={cn(cardVariants({ type }), className)}
+  onclick={onclick != null && preventDefault(onclick)}
+  {...attrs}
+>
+  {#if clickable}
+    <Layer />
+  {/if}
+  {@render children?.()}
+</Primitive>
 
 <style>
-  :root {
-    --m3-card-shape: var(--m3-util-rounding-medium);
-  }
+  /*:root {*/
+  /*  --m3-card-shape: var(--m3-util-rounding-medium);*/
+  /*}*/
 
-  .m3-container {
-    flex-direction: column;
-    position: relative;
-    padding: 1rem; /* protip: use margin: -1rem (adjust as needed) to make images stretch to the end */
-    border-radius: var(--m3-card-shape);
-    background-color: rgb(var(--m3-scheme-surface));
-    color: rgb(var(--m3-scheme-on-surface));
-  }
+  /*.m3-container {*/
+  /*  flex-direction: column;*/
+  /*  position: relative;*/
+  /*  padding: 1rem; !* protip: use margin: -1rem (adjust as needed) to make images stretch to the end *!*/
+  /*  border-radius: var(--m3-card-shape);*/
+  /*  background-color: rgb(var(--m3-scheme-surface));*/
+  /*  color: rgb(var(--m3-scheme-on-surface));*/
+  /*}*/
 
-  .type-elevated {
-    background-color: rgb(var(--m3-scheme-surface-container-low));
-  }
-  .type-filled {
-    background-color: rgb(var(--m3-scheme-surface-container-highest));
-  }
-  .type-outlined {
-    border: solid 0.0625rem rgb(var(--m3-scheme-outline-variant));
-  }
+  /*.type-elevated {*/
+  /*  background-color: rgb(var(--m3-scheme-surface-container-low));*/
+  /*}*/
 
-  .type-elevated {
-    box-shadow: var(--m3-util-elevation-1);
-  }
+  /*.type-filled {*/
+  /*  background-color: rgb(var(--m3-scheme-surface-container-highest));*/
+  /*}*/
 
-  @media print, (forced-colors: active) {
-    .type-filled {
-      outline: solid 0.125rem;
-    }
-  }
-  @media (forced-colors: active) {
-    .type-elevated {
-      outline: solid 0.125rem;
-    }
-  }
+  /*.type-outlined {*/
+  /*  border: solid 0.0625rem rgb(var(--m3-scheme-outline-variant));*/
+  /*}*/
+
+  /*.type-elevated {*/
+  /*  box-shadow: var(--m3-util-elevation-1);*/
+  /*}*/
+
+  /*@media print, (forced-colors: active) {*/
+  /*  .type-filled {*/
+  /*    outline: solid 0.125rem;*/
+  /*  }*/
+  /*}*/
+
+  /*@media (forced-colors: active) {*/
+  /*  .type-elevated {*/
+  /*    outline: solid 0.125rem;*/
+  /*  }*/
+  /*}*/
 </style>
--- src/routes/3.svelte
+++ src/routes/3.svelte
@@ -1,37 +1,36 @@
 <script lang="ts">
-import { createEventDispatcher } from "svelte";
-import iconCircle from "@ktibow/iconset-material-symbols/circle-outline";
-import iconSquare from "@ktibow/iconset-material-symbols/square-outline";
-import iconTriangle from "@ktibow/iconset-material-symbols/change-history-outline";
-import Switch from "$lib/forms/Switch.svelte";
-import Icon from "$lib/misc/_icon.svelte";
-import Arrows from "./_arrows.svelte";
-import InternalCard from "./_card.svelte";
-import Card from "$lib/containers/Card.svelte";
-import CardClickable from "$lib/containers/CardClickable.svelte";
-let type: "elevated" | "filled" | "outlined" = "elevated";
-let clickable = false;
+  import Switch from "$lib/forms/Switch.svelte";
+  import Arrows from "./_arrows.svelte";
+  import InternalCard from "./_card.svelte";
+  import Card from "$lib/containers/Card.svelte";
+  import CardClickable from "$lib/containers/CardClickable.svelte";
+  import type { DocProps } from "./+page.svelte";
 
-const dispatch = createEventDispatcher();
-const minimalDemo = `${"<"}Card type="filled">Hello${"<"}/Card>
-${"<"}CardClickable type="filled" on:click={() => alert("!")}>Hello${"<"}/CardClickable>`;
-const relevantLinks = [{"title":"Card.sv","link":"https://github.com/KTibow/m3-svelte/blob/main/src/lib/containers/Card.svelte"},{"title":"CardClickable.sv","link":"https://github.com/KTibow/m3-svelte/blob/main/src/lib/containers/CardClickable.svelte"}];
+  let type: "elevated" | "filled" | "outlined" = $state("elevated");
+  let clickable = $state(false);
+
+  const minimalDemo = `<Card type="filled">Hello</Card>
+<CardClickable type="filled" on:click={() => alert("!")}>Hello</CardClickable>`;
+  const relevantLinks = [{
+    "title": "Card.sv",
+    "link": "https://github.com/KTibow/m3-svelte/blob/main/src/lib/containers/Card.svelte",
+  },];
+
+  let { showCode }: DocProps = $props();
 </script>
 
-<InternalCard title="Card" on:showCode={() => dispatch("showCode", { name: "Card", minimalDemo, relevantLinks })}>
-<label>
-  <Arrows list={["elevated", "filled", "outlined"]} bind:value={type} />
-  {type[0].toUpperCase() + type.slice(1)}
-</label>
-<label>
-  <Switch bind:checked={clickable} />
-  {clickable ? "Clickable" : "Not clickable"}
-</label>
-<div slot="demo">
-  {#if clickable}
-    <CardClickable {type}>Hello</CardClickable>
-  {:else}
-    <Card {type}>Hello</Card>
-  {/if}
-</div>
+<InternalCard title="Card" showCode={() => showCode({ name: "Card", minimalDemo, relevantLinks })}>
+  <label>
+    <Arrows list={["elevated", "filled", "outlined"]} bind:value={type} />
+    {type[0].toUpperCase() + type.slice(1)}
+  </label>
+  <label>
+    <Switch bind:checked={clickable} />
+    {clickable ? "Clickable" : "Not clickable"}
+  </label>
+  {#snippet demo()}
+    <div>
+      <Card {clickable} {type}>Hello</Card>
+    </div>
+  {/snippet}
 </InternalCard>
