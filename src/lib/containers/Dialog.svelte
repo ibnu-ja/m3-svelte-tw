a455b30e698afe34e22d2644ec36e3c2e14feacb
Ibnu JA <ibnu.ja@outlook.com>
2025-05-04 09:45:15 +0000

    fix dialog animation

--- src/lib/containers/Dialog.svelte
+++ src/lib/containers/Dialog.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import type { IconifyIcon } from "@iconify/types";
   import { type Snippet } from "svelte";
   import type { HTMLDialogAttributes } from "svelte/elements";
   import { cn } from "$lib/misc/utils";
@@ -22,15 +21,15 @@
     else dialog.close();
   });
   type Props = {
-    open?: boolean
-    closeOnEsc?: boolean
-    closeOnClick?: boolean
-    onCloseByEsc?: () => void
-    onCloseByClick?: () => void
+    open?: boolean;
+    closeOnEsc?: boolean;
+    closeOnClick?: boolean;
+    onCloseByEsc?: () => void;
+    onCloseByClick?: () => void;
     children?: Snippet;
     class?: HTMLDialogAttributes["class"];
     [key: string]: any;
-  }
+  };
 
   let {
     open = $bindable(false),
@@ -48,20 +47,30 @@
 <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 <!--suppress HtmlDeprecatedAttribute -->
 <dialog
-  class={cn([
-    "m-auto",
-    "rounded-xl",
-    "bg-surface-container-high",
-    "border-none",
-    "min-w-70",
-    "max-w-140",
-    "p-0",
-    "overflow-auto",
-    "backdrop:bg-scrim/30",
-    "transition-all",
-    "backdrop:animate-[opacity_400ms]",
-    "open:animate-(--animate-dialog-in)",
-  ], className)}
+  class={cn(
+    [
+      "m-auto",
+      "rounded-xl",
+      "bg-surface-container-high",
+      "border-none",
+      "min-w-70",
+      "max-w-140",
+      "p-0",
+      "overflow-auto",
+      "backdrop:bg-scrim/30",
+      "transition-opacity",
+      "transition-visibility",
+      "backdrop:animate-[opacity_400ms]",
+      "open:animate-(--dialog-animation)",
+      "open:opacity-100",
+      "open:visible",
+      "pointer-events-auto",
+      "backdrop:bg-scrim/30",
+      "backdrop:animate-opacity",
+      "backdrop:duration-400",
+    ],
+    className,
+  )}
   oncancel={(e) => {
     if (closeOnEsc) {
       onCloseByEsc?.();
@@ -83,13 +92,29 @@
 </dialog>
 
 <style>
-    :root {
-        --animate-dialog-in: dialogIn 500ms var(--m3-easing-decel), opacity 100ms var(--m3-easing-decel);
-    }
+  :root {
+    --m3-dialog-shape: var(--m3-util-rounding-extra-large);
+    --dialog-animation: dialogIn 500ms var(--m3-easing-decel), opacity 100ms var(--m3-easing-decel);
+  }
 
-    @media print, (forced-colors: active) {
-        dialog {
-            outline: solid 0.125rem canvastext;
-        }
+  /*dialog[open] .headline {*/
+  /*  animation: opacity 150ms;*/
+  /*}*/
+
+  /*dialog[open] .content {*/
+  /*  animation: opacity 200ms;*/
+  /*}*/
+
+  /*dialog[open] .buttons {*/
+  /*  position: relative;*/
+  /*  animation:*/
+  /*    buttonsIn 500ms var(--m3-easing-decel),*/
+  /*    opacity 200ms 100ms backwards;*/
+  /*}*/
+
+  @media print, (forced-colors: active) {
+    dialog {
+      outline: solid 0.125rem canvastext;
     }
+  }
 </style>
--- src/routes/7.svelte
+++ src/routes/7.svelte
@@ -1,3 +1,4 @@
+<!--suppress JSXUnresolvedComponent -->
 <script lang="ts">
   import iconCircle from "@ktibow/iconset-material-symbols/circle-outline";
   import InternalCard from "./_card.svelte";
@@ -34,13 +35,13 @@
       <div class="m-auto flex flex-col p-6 w-full">
         <Icon icon={iconCircle} class="mb-4 shrink h-6 w-6 text-secondary self-center" />
         <!--headline/icon-->
-        <h4 class="m3-font-headline-small text-center text-on-surface mt-0 mb-4 animate-[opacity_150ms]">Hello</h4>
+        <h4 class="m3-font-headline-small text-center text-on-surface mt-0 mb-4 animate-opacity duration-150">Hello</h4>
         <!--content-->
-        <div class="grow m3-font-body-medium text-on-surface-variant mb-6 relative animate-[opacity_200ms]">
+        <div class="grow m3-font-body-medium text-on-surface-variant mb-6 relative animate-opacity duration-200">
           Anything is possible at ZomboCom! You can do anything at ZomboCom! The infinite is possible at
           ZomboCom! The unattainable is unknown at ZomboCom!
         </div>
-        <div class="flex justify-end gap-2 animation-[buttonsIn_500ms_var(--m3-easing-decel)] animate-[opacity_200ms_100ms_backwards]">
+        <div class="flex justify-end gap-2 animation-[buttonsIn_500ms_var(--m3-easing-decel),_opacity_200ms_100ms_backwards]">
           <Button type="text" onclick={() => (open = false)}>Cancel</Button>
           <Button type="text" onclick={() => (open = false)}>OK</Button>
         </div>
