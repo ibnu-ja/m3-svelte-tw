584bae062ddb767f3dee1475abf33fd06a2023d1
Ibnu JA <ibnu.ja@outlook.com>
2025-05-04 12:36:33 +0000

    update _icon.svelte

--- src/lib/misc/_icon.svelte
+++ src/lib/misc/_icon.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
   import type { IconifyIcon } from "@iconify/types";
-  export let icon: IconifyIcon;
-  export let width: string = "1em";
-  export let height: string = "1em";
-  let clazz: string | undefined = undefined;
-  export { clazz as class };
+  import type { Snippet } from "svelte";
+  import type { SvelteHTMLElements } from "svelte/elements";
+  import { cn } from "$lib/misc/utils";
+
+  type Props = {
+    icon: IconifyIcon
+    width?: string
+    height?: string
+    children?: Snippet;
+    class?: SvelteHTMLElements["svg"]["class"];
+    [key: string]: any;
+  };
+
+  let { icon, width = "1em", height = "1em", class: className }: Props = $props();
 </script>
 
-<svg {width} {height} class={clazz} viewBox="0 0 {icon.width} {icon.height}">
+<svg {width} {height} class={cn(className)} viewBox="0 0 {icon.width} {icon.height}">
   {@html icon.body}
 </svg>
