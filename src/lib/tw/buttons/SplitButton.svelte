99d91c454b96d660571c98bac7a808113fe01cb0
ibnu-ja <ibnu.ja@outlook.com>
2026-03-29 02:00:53 +0700

    fix split button animation/icon rotation

--- src/lib/tw/buttons/SplitButton.svelte
+++ src/lib/tw/buttons/SplitButton.svelte
@@ -32,12 +32,7 @@
 </script>
 
 <div class="inline-grid grid-cols-[1fr_auto] gap-[0.125rem] [print-color-adjust:exact]">
-  <Button
-    {variant}
-    {size}
-    class="rounded-ss-[1.25rem] rounded-es-[1.25rem] rounded-se-xs rounded-ee-xs hover:rounded-se-xl hover:rounded-ee-xl active:rounded-se-xl active:rounded-ee-xl"
-    {...props}
-  >
+  <Button {variant} {size} class="rounded-ss-[1.25rem] rounded-es-[1.25rem] rounded-se-xs rounded-ee-xs hover:rounded-se-xl hover:rounded-ee-xl !active:rounded-ss-[1.25rem] !active:rounded-es-[1.25rem] !active:rounded-se-xl !active:rounded-ee-xl ![transition:border-start-end-radius_var(--m3-easing-fast),border-end-end-radius_var(--m3-easing-fast),box-shadow_var(--m3-easing-fast)]" {...props}>
     {@render children()}
   </Button>
 
@@ -47,10 +42,13 @@
         <Button
           {variant}
           {size}
-          class="w-12 rounded-ss-xs rounded-es-xs rounded-se-[1.25rem] rounded-ee-[1.25rem] hover:rounded-ss-xl hover:rounded-es-xl active:rounded-ss-xl active:rounded-es-xl data-[state=open]:rounded-ss-xs data-[state=open]:rounded-es-xs [&>svg]:transition-transform [&>svg]:ease-m3-fast [&>svg]:duration-m3-fast data-[state=open]:[&>svg]:rotate-180"
+          class="w-12 rounded-ss-xs rounded-es-xs rounded-se-[1.25rem] rounded-ee-[1.25rem] hover:rounded-ss-xl hover:rounded-es-xl !active:rounded-ss-xl !active:rounded-es-xl !active:rounded-se-[1.25rem] !active:rounded-ee-[1.25rem] data-[state=open]:rounded-ss-[1.25rem] data-[state=open]:rounded-es-[1.25rem] ![transition:border-start-start-radius_var(--m3-easing-fast),border-end-start-radius_var(--m3-easing-fast),box-shadow_var(--m3-easing-fast)]"
           {...triggerProps}
         >
-          <Icon icon={iconExpand} size={22} />
+          <!-- FIXME maybe wrong -->
+          <span class="inline-flex items-center justify-center [transition:rotate_var(--m3-easing-fast),translate_var(--m3-easing-fast)] translate-x-[-1.76px] [button:hover_&]:translate-x-[-0.88px] [button:active_&]:translate-x-[-0.88px] [[data-state=open]_&]:translate-x-0 [[data-state=open]_&]:rotate-180">
+            <Icon icon={iconExpand} size={22} />
+          </span>
         </Button>
       {/snippet}
     </DropdownMenu.Trigger>
